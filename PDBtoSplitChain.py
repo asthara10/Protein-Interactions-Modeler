8b999a77f08c845426634c4520d0228812146532
helenarodllo <33523509+helenarodllo@users.noreply.github.com>
2018-03-13 18:45:50 +0100

    Add files via upload

--- PDBtoSplitChain.py
+++ PDBtoSplitChain.py
@@ -1,19 +1,40 @@
-from Bio.PDB import PDBParser, PDBIO, PPBuilder
-import sys
+import argparse
 
-file = sys.argv[1]
+def SplitChain(infiles):
+	"""
+	blah
+	"""
+	chain_names = set()
+	for file in options.infiles:
+		name = file.split(".")[0]
+		io = PDBIO()
+		pdb = PDBParser().get_structure(name, file)
+		polipeptide = PPBuilder()
+		for chain in pdb.get_chains():
+			print (chain.get_id())
+			if chain.get_id() not in chain_names:
+				io.set_structure(chain)
+				io.save(pdb.get_id() + "_" + chain.get_id() + ".pdb")
 
-io = PDBIO()
-pdb = PDBParser().get_structure(file)
-polipeptide = PPBuilder()
+				for pp in polipeptide.build_peptides(pdb):
+					fasta = open(pdb.get_id() + "_" + chain.get_id() + ".fa", "w")
+					fasta.write(">" + pdb.get_id() + "_" + chain.get_id() + "\n")
+					fasta.write(str(pp.get_sequence()))
+			chain_names.add(chain.get_id())
 
-for chain in pdb.get_chains():
-	io.set_structure(chain)
-	io.save(pdb.get_(id) + "_" + chain.get_id() + ".pdb")
+if __name__ == "__main__":
 
-for pp in polipeptide.build_peptides(pdb):
-	fasta = open(pdb.get_(id) + "_" + chain.get_id() + ".fa", "w")
-	fasta.write(">" + pdb.get_(id) + "_" + chain.get_id() + "\n")
-	fasta.write(pp.get_sequence())
-		
+	from Bio.PDB import PDBParser, PDBIO, PPBuilder
 
+	parser = argparse.ArgumentParser(description="blah")
+
+	parser.add_argument('-i', '--input',
+				dest= "infiles",
+				action= "store",
+				required = True,
+				help="blah",
+				nargs = "+")
+
+	options=parser.parse_args()
+
+	SplitChain(options.infiles)
