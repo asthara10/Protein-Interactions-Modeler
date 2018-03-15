1282cdf879bf86a14c7acb0d238127c81d0728e7
julia <mirp.julia@gmail.com>
2018-03-15 18:42:41 +0100

    clustal implemented

---
+++ All_in_One.py
@@ -0,0 +1,44 @@
+import argparse
+
+def Parse_PDB(infiles):
+	
+
+
+def SplitChain(infiles):
+	"""
+	blah
+	"""
+	chain_names = set()
+	for file in options.infiles:
+		name = file.split(".")[0]
+		io = PDBIO()
+		pdb = PDBParser().get_structure(name, file)
+		polipeptide = PPBuilder()
+		for chain in pdb.get_chains():
+			print (chain.get_id())
+			if chain.get_id() not in chain_names:
+				io.set_structure(chain)
+				io.save(pdb.get_id() + "_" + chain.get_id() + ".pdb")
+
+				for pp in polipeptide.build_peptides(pdb):
+					fasta = open(pdb.get_id() + "_" + chain.get_id() + ".fa", "w")
+					fasta.write(">" + pdb.get_id() + "_" + chain.get_id() + "\n")
+					fasta.write(str(pp.get_sequence()))
+			chain_names.add(chain.get_id())
+
+if __name__ == "__main__":
+
+	from Bio.PDB import PDBParser, PDBIO, PPBuilder
+
+	parser = argparse.ArgumentParser(description="blah")
+
+	parser.add_argument('-i', '--input',
+				dest= "infiles",
+				action= "store",
+				required = True,
+				nargs = "+",
+				help="input file names (pdb files with pairs of chains that interact), insert as many as needed.")
+
+	options=parser.parse_args()
+
+	SplitChain(options.infiles)
--- Modelling.py
+++ Modelling.py
@@ -5,33 +5,49 @@ from Bio.PDB import PDBParser, PDBIO, PPBuilder
 
 input_files = ["AB.pdb", "AC.pdb", "AD.pdb", "BC.pdb", "DC.pdb"]
 Blast_outs = ["AB_A.fa.xml", "AB_B.fa.xml", "AD_D.fa.xml", "AC_C.fa.xml"]
-#input_fasta = ["AB_A.fa", "AB_B.fa", "AD_D.fa", "AC_C.fa"]
+input_fasta = ["AB_A.fa", "AB_B.fa", "AD_D.fa", "AC_C.fa"]
 templates = Blast_align.Select_template(Blast_outs)
 
 templates = list(templates)
 #templates = [x.upper() for x in templates]
+def Create_Fastas(templates, input_files):
+	pdbl = PDBList()
+	io = PDBIO()
+	polipeptide = PPBuilder()
+	#for inputfa in input_files:
 
-pdbl = PDBList()
-io = PDBIO()
-polipeptide = PPBuilder()
-#for inputfa in input_files:
-
-for template in templates:
-	pdbl.retrieve_pdb_file(template, pdir="./", file_format='pdb', obsolete=False)#al fer maco der tot mb carpetes
-	pdb = PDBParser().get_structure(template, "pdb"+template+".ent")
-	for infa in input_files:
-		pd1 = PDBParser().get_structure(infa.split(".")[0], infa)
+	for template in templates:
+		first_temp = True
+		#pdbl.retrieve_pdb_file(template, pdir="./", file_format='pdb', obsolete=False)#al fer maco der tot mb carpetes
+		pdb = PDBParser().get_structure(template, "./ent/pdb"+template+".ent")
 		for pp in polipeptide.build_peptides(pdb):
-			for qq in polipeptide.build_peptides(pd1):
-				new_fasta = open(template + "_" + infa.split(".")[0]+".fa", "w")
+			if first_temp:
+				new_fasta = open(template + "_paired.fa", "w")
 				new_fasta.write(">" + template +"\n")
 				new_fasta.write(str(pp.get_sequence()))
-				new_fasta.write("\n>" + infa.split(".")[0] +"\n")
-				new_fasta.write(str(qq.get_sequence()))
+				first_temp = False
+			else:
+				new_fasta.write(str(pp.get_sequence()))
+		for infa in input_files:
+			first_chain = True
+			new_fasta = open(template + "_paired.fa", "a")
+			pd1 = PDBParser().get_structure(infa.split(".")[0], infa)
+			for qq in polipeptide.build_peptides(pd1):
+				if first_chain:
+					new_fasta.write("\n>" + infa.split(".")[0] +"\n")
+					new_fasta.write(str(qq.get_sequence()))
+					first_chain = False
+				else:
+					new_fasta.write(str(qq.get_sequence()))
 	
 
+fastas = ["1a0u_paired.fa","1a0z_paired.fa","1dxt_paired.fa","1dxu_paired.fa","1gli_paired.fa","1j7s_paired.fa","1o1l_paired.fa","1o1n_paired.fa","1y0t_paired.fa","1y0w_paired.fa"]
 
+def Run_clustal(fastas):
+	for fasta in fastas:
+		#aln = open(fasta.split(".")[0]+".aln", "w")
+		clustalw_cline = ClustalwCommandline("clustalw2", infile=fasta)
+		clustalw_cline()
 
-def Run_clustal(input_files):
-	for file in input_files:
-		cline = ClustalwCommandline("clustalw2", infile=file)
+Create_Fastas(templates, input_files)
+Run_clustal(fastas)
